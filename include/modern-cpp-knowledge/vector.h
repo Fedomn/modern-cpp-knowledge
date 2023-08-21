769f985ae822bf9b3a26b4daac6314e0147a6b2d
Fedomn <fedomn.ma@gmail.com>
2023-08-21 21:34:34 +0800

    add copy/move constructor and assignment

--- include/modern-cpp-knowledge/vector.h
+++ include/modern-cpp-knowledge/vector.h
@@ -12,15 +12,53 @@ namespace vector
     int sz;
 
    public:
+    Vector() = default;
+
     explicit Vector(int size);
 
     // initialize with a list of doubles
     Vector(std::initializer_list<T> list);
 
-    Vector(const Vector& input);             // copy constructor
-    Vector& operator=(const Vector& input);  // copy assignment
-    Vector(Vector&& input);                  // move constructor
-    Vector& operator=(Vector&& input);       // move assignment
+    Vector(const Vector& input)
+    {
+      elem = new T[static_cast<unsigned long>(input.sz)];
+      sz = input.sz;
+      puts("copy constructor");
+      for (int i = 0; i < sz; ++i)
+      {
+        elem[i] = input.elem[i];
+      }
+    };  // copy constructor
+    Vector& operator=(const Vector& input)
+    {
+      puts("copy assignment");
+      T* p = new T[static_cast<unsigned long>(input.sz)];
+      for (int i = 0; i < input.sz; ++i)
+      {
+        p[i] = input.elem[i];
+      }
+      delete[] elem;
+      elem = p;
+      sz = input.sz;
+      return *this;
+    };  // copy assignment
+
+    Vector(Vector&& input) : elem(input.elem), sz(input.sz)
+    {
+      puts("move constructor");
+      input.elem = nullptr;
+      input.sz = 0;
+    };  // move constructor
+    Vector& operator=(Vector&& input)
+    {
+      puts("move assignment");
+      delete[] elem;
+      elem = input.elem;
+      sz = input.sz;
+      input.elem = nullptr;
+      input.sz = 0;
+      return *this;
+    };  // move assignment
 
     ~Vector()
     {
--- test/src/vector_test.cpp
+++ test/src/vector_test.cpp
@@ -16,4 +16,20 @@ TEST(VectorTest, Test)
   vector2[2] = 4.1;
   EXPECT_EQ(vector2[2], 4.1);
   EXPECT_EQ(vector2.size(), 3);
+
+  // copy constructor
+  auto vector3 = Vector<int>{ vector };
+  EXPECT_EQ(vector3[0], 1);
+  // copy assignment
+  Vector<int> vector4{};
+  vector4 = vector3;
+  EXPECT_EQ(vector4[0], 1);
+
+  // move constructor
+  auto vector5 = Vector<int>{ std::move(vector4) };
+  EXPECT_EQ(vector5[0], 1);
+  // move assignment
+  Vector<int> vector6{};
+  vector6 = std::move(vector5);
+  EXPECT_EQ(vector6[0], 1);
 }
